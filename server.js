c0a51d2c09324b74bb183a62a17ab578c2a036fc
uchiha5555 <innocentdev0512@gmail.com>
2023-11-21 04:03:15 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+
+const fs = require("fs");
+
+const { removeBackground } = require("@imgly/background-removal-node");
+
+const app = express();
+
+app.use(cors("*"));
+
+const storageEngine = multer.diskStorage({
+  destination: "./images",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}--${file.originalname}`);
+  },
+});
+
+const upload = multer({
+  storage: storageEngine,
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("processed"));
+
+const uploadFiles = async (req, res) => {
+  const image = req.file;
+
+  if (!image) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  console.log(image);
+
+  removeBackground(image.path).then(async (blob) => {
+    console.log(blob);
+    const bufferData = Buffer.from(await blob.arrayBuffer());
+
+    const filePath =
+      image.filename.substring(0, image.filename.lastIndexOf(".")) + ".png";
+
+    fs.writeFile("processed\\" + filePath, bufferData, (err) => {
+      if (err) {
+        console.log("Error saving PNG file:", err);
+      } else {
+        console.log("PNG file saved successfully!");
+      }
+    });
+    res.json({ imageUrl: filePath });
+  });
+};
+
+app.post("/upload_files", upload.single("image"), uploadFiles);
+
+app.listen(5000, () => {
+  console.log("Server started....");
+});
